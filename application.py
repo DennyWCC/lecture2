0f1de92f9da6fe223f7d58f51f78e7584d7b351c
DennyWCC <dennywcc@gmail.com>
2019-10-10 00:20:06 +0800

    Update application.py
    Added dynamic content based on 'headline' variable. 'htmlheadline' is the name of the text template in file 'index.html'

--- application.py
+++ application.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#dynamic content based on 'headline' variable
 @app.route("/")
 def index():
-    return render_template("index.html")
+    headline = "Hello World!"
+    return render_template("index.html", htmlheadline=headline)
+
+#dinamic content for "/bye" route based on 'headline' variable
+@app.route("/bye")
+def bye():
+    headline = "Goodbye!"
+    return render_template("index.html", htmlheadline=headline)
 
 #print Hello World in webpage
 '''@app.route("/")
