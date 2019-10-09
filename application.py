5840f3f0fc55d7b8378746216464d6d768ab2ad5
DennyWCC <dennywcc@gmail.com>
2019-10-10 00:01:36 +0800

    Update application.py
    Added render_template libraries

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 #print Hello World in webpage
 '''@app.route("/")
@@ -9,7 +13,8 @@ def index():
 '''
 
 #dynamic function
-@app.route("/<string:name>")
+'''@app.route("/<string:name>")
 def hello(name):
-    name = name.capitalize()
+    name = name.capitalize() #capitalize first letter
     return f"Hello {name}!"
+'''
