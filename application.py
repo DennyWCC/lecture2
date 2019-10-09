9154a3aa66dc3d10b43b1d11dd81a0cad9f95ccb
DennyWCC <dennywcc@gmail.com>
2019-10-09 23:52:23 +0800

    Update application.py
    Added parameter capitalize function

--- application.py
+++ application.py
@@ -2,10 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+#print Hello World in webpage
+'''@app.route("/")
 def index():
     return "Hello World!"
+'''
 
+#dynamic function
 @app.route("/<string:name>")
 def hello(name):
+    name = name.capitalize()
     return f"Hello {name}!"
